53c2252870089ddb0247bb070757a88c7a69ffed
D1mitryS <D1mitryS@pm.me>
2022-04-15 23:01:23 +0300

    gif on load added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* name generator */ 
-
 const decoration = [
     "宏",
     "牡",
@@ -18,7 +17,6 @@ const decoration = [
     "☠",
     "༒",
 ];
-
 const prefix = [
     "zxc",
     "ZXC",
@@ -35,13 +33,11 @@ const prefix = [
     "322",
     "ugly god",
 ]
-
 const spacing = [
     " ",
     " ",
     "|"
 ]
-
 const phrase = [
     "мёртв внутри",
     "дед внутри",
@@ -63,7 +59,6 @@ const phrase = [
     "bury me alive",
     "1v9"
 ]
-
 const word = [
     "hopeless",
     "pain",
@@ -85,7 +80,6 @@ const word = [
     "tragic",
     "emptiness"
 ]
-
 const mapping = [
     "randomPrefix + randomSpacing + randomPhrase + randomDecoration + randomWord +randomDecoration",
     "randomPrefix + randomDecoration + randomPhrase + randomDecoration + randomWord",
@@ -125,48 +119,38 @@ generatorButton.addEventListener('click', function () {
     let randomPhrase = getRandomElement(phrase);
     let randomWord = getRandomElement(word);
     let randomMaping = getRandomElement(mapping);
-    generatorName.textContent = eval(randomMaping);
     /* turns string from "mapping" array into variable */
-    if (generatorName.textContent.length >= 15) {
+    generatorName.textContent = eval(randomMaping);
+    let nameLength = generatorName.textContent.length
+    if (nameLength >= 15 && nameLength < 25) {
         generatorName.style.fontSize = "2.5em";
         generatorName.style.fontWeight = "500";
+    }   else if (nameLength >= 25) {
+        generatorName.style.fontSize = "2em";
+        generatorName.style.fontWeight = "500";
     }   else {
-            generatorName.style.fontSize = "3em";
-            generatorName.style.fontWeight = "400"
+        generatorName.style.fontSize = "3em";
+        generatorName.style.fontWeight = "normal"
     }
 })
 
-
-/* image switcher and audio play */
-
-
-const images = [
-    { src:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpm1.narvii.com%2F6699%2F701699184917adb74894b7b31772d54408e06b36_00.jpg&f=1&nofb=1" , alt:"уже почти гуль" },
-    { src:"https://awesomereviews.ru/wp-content/uploads/2016/03/miniature-752x440.jpg" , alt:"гуль" },
-    { src:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmemepedia.ru%2Fwp-content%2Fuploads%2F2019%2F08%2Fded-insayd-5-768x768.jpg&f=1&nofb=1" , alt:"гуль валакас" },
-    { src:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fsun9-36.userapi.com%2Fimpg%2Fw73PxsgiyLLHCr24SKWw0eYVD7FNl2JeywsHOA%2FQ9K1hPup7ts.jpg%3Fsize%3D400x0%26quality%3D90%26crop%3D280%2C0%2C720%2C720%26sign%3D2290780159d08078773ab63707d815b7%26ava%3D1&f=1&nofb=1" , alt:"безобидный гуль" },
-]
-
-
-let generatorImage = document.querySelector('.generator-image');
-let audio = document.querySelector('.audio');
-let generatorCounter = 0;
+/* audio play */
+const audio = document.querySelector('.audio');
 
 generatorButton.addEventListener('click', function () {
     audio.play();
-    generatorCounter += 1;
-    if (generatorCounter >= 3 && generatorCounter < 6) {
-            generatorImage.src = images[0].src;
-            generatorImage.alt = images[0].alt;
-    }   else if (generatorCounter >= 6 && generatorCounter < 9) {
-            generatorImage.src = images[1].src;
-            generatorImage.alt = images[1].alt;
-    }   else if (generatorCounter >= 9 && generatorCounter < 12)  {
-            generatorImage.src = images[2].src;
-            generatorImage.alt = images[2].alt;
-    }   else if (generatorCounter >= 12) {
-            generatorImage.src = images[images.length - 1].src;
-            generatorImage.alt = images[images.length - 1].alt;
-            generatorCounter = 0;
-    }
 })
+
+/* image switcher */
+const images = [
+    "img/giphy0.gif",
+    "img/giphy1.gif",
+    "img/giphy2.gif",
+    "img/giphy3.gif",
+    "img/giphy4.gif",
+]
+
+let generatorImage = document.querySelector('.generator-image');
+
+generatorImage.src = getRandomElement(images);
+
